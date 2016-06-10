4dee40c6fd76c57b7aa867ab4ac5b5eb522bcdac
brambossche <bramvdbossche@gmail.com>
2016-06-10 13:44:14 +0200

    added new form and stashed test

--- GitTestSolution/GitTestSolution/Form1.Designer.cs
+++ GitTestSolution/GitTestSolution/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             // button2
             // 
+
             this.button2.Location = new System.Drawing.Point(69, 86);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
