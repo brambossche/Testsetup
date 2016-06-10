7b1ae42f42840496a890a0e0d31a7101125423e8
brambossche <bramvdbossche@gmail.com>
2016-06-10 14:06:12 +0200

    Branch2 (#2)
    * adding date time
    
    * added new form and stashed test

---
+++ GitTestSolution/GitTestSolution/frm_branch2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTestSolution
+{
+    public partial class frm_branch2 : Form
+    {
+        public frm_branch2()
+        {
+            InitializeComponent();
+        }
+    }
+}
